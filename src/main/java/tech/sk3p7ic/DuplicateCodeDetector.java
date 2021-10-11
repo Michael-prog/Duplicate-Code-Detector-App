5211bf6ade82ec3316bb3870196956a199173cce
sk3p7c <joshuaibrom@gmail.com>
2021-10-11 06:09:35 -0500

    Worked on using command-line arguments.

--- src/main/java/tech/sk3p7ic/DuplicateCodeDetector.java
+++ src/main/java/tech/sk3p7ic/DuplicateCodeDetector.java
@@ -2,6 +2,9 @@ package tech.sk3p7ic;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
+import tech.sk3p7ic.cli.CliOptions;
+import tech.sk3p7ic.detector.Detector;
 import tech.sk3p7ic.detector.files.FileIndexPair;
 import tech.sk3p7ic.detector.files.SourceFile;
 
@@ -15,11 +18,16 @@ public class DuplicateCodeDetector {
     //logger.error("Hello world!");
     //SourceFile sourceFile = new SourceFile("C:\\Users\\joshu\\IdeaProjects\\Duplicate-Code-Detector-App\\src\\main\\java\\tech\\sk3p7ic\\detector\\files\\FileReader.java");
     //SourceFile sourceFile = new SourceFile("C:\\Users\\joshu\\IdeaProjects\\Duplicate-Code-Detector-App\\src\\main\\java\\tech\\sk3p7ic\\DuplicateCodeDetector.java");
-    SourceFile sourceFile = new SourceFile("C:\\Users\\Josh\\IdeaProjects\\Duplicate-Code-Detector-App\\src\\main\\java\\tech\\sk3p7ic\\detector\\files\\FileReader.java");
-    sourceFile.generateAll();
-    List<FileIndexPair> fileIndexPairs = sourceFile.getFileIndexPairs();
-    for (FileIndexPair pair : fileIndexPairs) {
-      System.out.println(pair.fileIndexType + ": (" + (pair.indexStart + 1) + ", " + (pair.indexEnd + 1) + ")");
-    }
+    //SourceFile sourceFile = new SourceFile("C:\\Users\\Josh\\IdeaProjects\\Duplicate-Code-Detector-App\\src\\main\\java\\tech\\sk3p7ic\\detector\\files\\FileReader.java");
+    //sourceFile.generateAll();
+    //List<FileIndexPair> fileIndexPairs = sourceFile.getFileIndexPairs();
+    //for (FileIndexPair pair : fileIndexPairs) {
+    //  System.out.println(pair.fileIndexType + ": (" + (pair.indexStart + 1) + ", " + (pair.indexEnd + 1) + ")");
+    //}
+    CliOptions cliOptions = new CliOptions();
+    new CommandLine(cliOptions).parseArgs(args);
+    System.out.println("Performing detection on '" + cliOptions.file1.getAbsolutePath() + "'");
+    Detector mainDetector = new Detector(cliOptions.file1, cliOptions.file1);
+    mainDetector.generateSimilarityScores(mainDetector.readFilesForItems());
   }
 }
--- src/main/java/tech/sk3p7ic/cli/CliOptions.java
+++ src/main/java/tech/sk3p7ic/cli/CliOptions.java
@@ -2,10 +2,15 @@ package tech.sk3p7ic.cli;
 
 import picocli.CommandLine;
 
+import java.io.File;
+
 public class CliOptions {
+  @CommandLine.Parameters(paramLabel = "File 1", description = "The first file to scan.")
+  public File file1;
+
   @CommandLine.Option(names = {"-c", "--createProject"}, description = "Create a project file for the input files.")
-  boolean createProject;
+  public boolean createProject;
 
   @CommandLine.Option(names = {"-h", "--help"}, usageHelp = true, description = "Display help message.")
-  private boolean helpRequested = false;
+  public boolean helpRequested = false;
 }
