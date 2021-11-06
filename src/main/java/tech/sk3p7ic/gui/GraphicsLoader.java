f34c60f10927512ae8549be63cd5cd4557e79ea1
sk3p7ic <joshuaibrom@gmail.com>
2021-11-06 10:18:07 -0500

    Started working on the GUI.

---
+++ src/main/java/tech/sk3p7ic/gui/GraphicsLoader.java
@@ -0,0 +1,9 @@
+package tech.sk3p7ic.gui;
+
+import tech.sk3p7ic.gui.views.MainView;
+
+public class GraphicsLoader {
+    public GraphicsLoader() {
+        MainView mainView = new MainView();
+    }
+}
---
+++ src/main/java/tech/sk3p7ic/gui/views/MainView.java
@@ -0,0 +1,13 @@
+package tech.sk3p7ic.gui.views;
+
+import javax.swing.*;
+
+public class MainView extends JFrame {
+    public MainView() {
+        this.setTitle("Duplicate Code Detector");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(1280, 780);
+        this.setVisible(true);
+    }
+}
