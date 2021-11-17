deb8c9c5ae13e0c95f7f0a44b012d3bea3b0138d
sk3p7c <joshuaibrom@gmail.com>
2021-11-16 19:41:36 -0600

    Added new color.

--- src/main/java/tech/sk3p7ic/gui/res/AppColors.java
+++ src/main/java/tech/sk3p7ic/gui/res/AppColors.java
@@ -4,7 +4,8 @@ public enum AppColors {
   MAIN_BG_DARK(0x263238),
   MAIN_FG_DARK(0xECEFF1),
   BG_DARK_0(0x37474f),
-  ACCENT_0(0xF50057);
+  ACCENT_0(0xF50057),
+  ACCENT_1(0xEC407A);
 
   private final int colorValue; // The value for the color
 
