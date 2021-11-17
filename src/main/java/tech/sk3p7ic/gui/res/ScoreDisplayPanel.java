ee8f9e42c392073e9e01d41c50c9a782408aea4e
sk3p7c <joshuaibrom@gmail.com>
2021-11-17 06:04:12 -0600

    File titles now displayed.

--- src/main/java/tech/sk3p7ic/gui/res/ScoreDisplayPanel.java
+++ src/main/java/tech/sk3p7ic/gui/res/ScoreDisplayPanel.java
@@ -6,11 +6,11 @@ import tech.sk3p7ic.detector.detection.SimilarityScore;
 import tech.sk3p7ic.detector.detection.SimilarityScoreManager;
 
 import javax.swing.*;
-import java.util.List;
+import java.awt.*;
 import java.util.Map;
 
 public class ScoreDisplayPanel extends JPanel {
-  private final int filenumber; // Stores whether this pane is for the first or second file in a SimilarityScore
+  private final int fileNumber; // Stores whether this pane is for the first or second file in a SimilarityScore
   private final String filename;
   private final String fileIndexType;
   private final SimilarityScore score;
@@ -21,7 +21,7 @@ public class ScoreDisplayPanel extends JPanel {
     score = scoreManager.getSimilarityScoreList().get(scoreId); // Get the score
     if (filenumber != 1 && filenumber != 2)
       throw new IllegalArgumentException("Invalid number. Must be 1 or 2.");
-    this.filenumber = filenumber;
+    this.fileNumber = filenumber;
     // Get the lines from the source file
     scoreFileLines = scoreManager.getSimilarityScoreLines(scoreId).get(filenumber - 1);
     // Get the filename
@@ -29,10 +29,12 @@ public class ScoreDisplayPanel extends JPanel {
     // Get the FileIndexType
     fileIndexType = (filenumber == 1) ? score.fileIndexPair1.fileIndexType.toString()
                                       : score.fileIndexPair2.fileIndexType.toString();
-    this.add(getTextPane(filenumber));
+    this.setBackground(new Color(AppColors.MAIN_BG_DARK.getColor()));
+    this.add(getFileTitleLabel());
+    this.add(getTextPane());
   }
 
-  public RTextScrollPane getTextPane(int fileNumber) {
+  public RTextScrollPane getTextPane() {
     RSyntaxTextArea textArea = new RSyntaxTextArea(60, 120);
     textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
     textArea.setCodeFoldingEnabled(true);
@@ -46,5 +48,16 @@ public class ScoreDisplayPanel extends JPanel {
     scrollPane.getGutter().setLineNumberingStartIndex((fileNumber == 1) ? score.fileIndexPair1.indexStart + 1
                                                                         : score.fileIndexPair2.indexStart + 1);
     return scrollPane;
+  }
+
+  public JLabel getFileTitleLabel() {
+    JLabel titleLabel = new JLabel();
+    titleLabel.setForeground(new Color(AppColors.MAIN_FG_DARK.getColor()));
+    int lineStart = (fileNumber == 1) ? score.fileIndexPair1.indexStart + 1 : score.fileIndexPair2.indexStart + 1;
+    int lineEnd = (fileNumber == 1) ? score.fileIndexPair1.indexEnd + 1 : score.fileIndexPair2.indexEnd + 1;
+    String titleText = String.format("%s (%5d, %-5d) :: %s -- %.2f", filename, lineStart, lineEnd, fileIndexType,
+        score.similarityScore);
+    titleLabel.setText("<html><h2>" + titleText + "</h2></html>");
+    return titleLabel;
   }
 }
